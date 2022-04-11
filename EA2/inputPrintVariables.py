af1f2a5d5fce96c9d277de28218ea576483a326d
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-11 11:42:47 -0300

    ae2

---
+++ EA2/inputPrintVariables.py
@@ -0,0 +1,14 @@
+edad = 18
+apellido = "jeager"
+print("Hola Eren", apellido, edad, 25)
+print("Ingrese un numero:")
+numero = int(input())  # int() convierte un texto en numero
+print("El nro ingresado es: ", numero)
+numero = numero + 100
+print("La suma es: ", numero)
+print("Ingrese su nombre:")
+nombre = input() # siempre devuelve un texto
+print("Su nombre es:", nombre)
+
+apellido = input("Ingrese su apellido:")
+print("Su apellido es:", apellido)
