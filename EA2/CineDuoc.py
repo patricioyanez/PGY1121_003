a15bad2db86ca192981894026862a06635093df8
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-20 18:26:46 -0400

    if

---
+++ EA2/CineDuoc.py
@@ -0,0 +1,24 @@
+print("**********  Cine Duoc *********")
+
+print("UD es de DUOC UC PLAZA OESTE de CERRILLOS de Americo Vespucio 1501")
+esDuonino = input("presione 1.- SI  2.- No")
+
+print("Tipo de entrada")
+print("1.- Estreno")
+print("2.- Normal")
+tipoEntrada = input("Seleccione el tipo de entrada:")
+
+
+print("Tamaño de las palomitas")
+print("1.- Pequeña")
+print("2.- Mediana")
+print("3.- Grande")
+tamonoPalomitas = input("Seleccione el tamaño:")
+
+print("Tamaño de la Bebida")
+print("1.- Pequeña")
+print("2.- Mediana")
+print("3.- Grande")
+tamanoBebida = input("Seleccione el tamaño:")
+
+## agregar los if para hacer el cobro
--- EA2/EjemploSentenciaIf2.py
+++ EA2/EjemploSentenciaIf2.py
@@ -2,3 +2,25 @@ nota1 = input("Ingrese nota 1: ")
 nota2 = input("Ingrese nota 2: ")
 nota3 = input("Ingrese nota 3: ")
 
+# convertir a numero decimal (float)
+
+nota1 = float(nota1)
+nota2 = float(nota2)
+nota3 = float(nota3)
+
+if nota1 < 1 or nota1 > 7:
+    print("Nota 1 no esta dentro del rango de notas")
+elif nota2 < 1 or nota2 > 7:
+    print("Nota 2 no esta dentro del rango de notas")
+elif nota3 < 1 or nota3 > 7:
+    print("Nota 3 no esta dentro del rango de notas")
+else: # las notas son válidas
+    promedio = (nota1 + nota2 + nota3) / 3
+    print("Su promedio es:", "{:0.1f}".format(promedio))
+
+    if promedio < 4:
+        print("Reprobó, no vimo el otro semestre")
+    else:
+        print("Aprobó")
+
+print("***** programa cerrado")
