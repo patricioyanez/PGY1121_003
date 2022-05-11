5e7f0385c19e2f6bf65fdbfde7728225273a730a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-11 14:32:56 -0400

    for

---
+++ EA3/EjemploFor1.py
@@ -0,0 +1,6 @@
+x = range(101) ## una secuencia del 0 al 100
+for n in x:
+    print("EL número es: ", n)
+
+for n in range(10):
+    print("***** El número es: ", n)
---
+++ EA3/EjemploFor2.py
@@ -0,0 +1,3 @@
+secuencia = range(0, 11, 3)
+for nro in secuencia:
+    print(nro)
